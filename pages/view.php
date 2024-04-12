327ec45d96d2128f730188c689959d0216966151
Seraf-seraf <130050600+Seraf-seraf@users.noreply.github.com>
2024-04-12 21:45:18 +0600

    Update view.php

--- pages/view.php
+++ pages/view.php
@@ -3,9 +3,13 @@ require_once '../functions/helper.php';
 
 $contacts = json_decode(file_get_contents('../data/data.json'), true);
 
-$id = strip_tags(htmlspecialchars($_GET['id']));
+$id = htmlspecialchars($_GET['id']);
 
 $contact = $contacts[$id];
+
+if (!$contact) {
+    header('Location: /pages/contacts.php');
+}
 
 $content = include_template('view.php', [
     'contact' => $contact
